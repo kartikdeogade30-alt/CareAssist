e24b79d34d141fc9ad52672bf0cb45e3d2d78e0a
Kartik Suhas Deogade <kartikdeogade30@gmail.com>
2026-01-22 01:13:08 +0530

    Added Back switch to Home page and Added Admin role in Login roles and removed Doctor role from Sign up

--- pages/Login.py
+++ pages/Login.py
@@ -1,20 +1,23 @@
 import streamlit as st
-import mysql.connector
+from database.db_connection import get_connection
 
-conn = mysql.connector.connect(
-            host = "localhost",
-            user = "root",
-            password = "dbda",
-            database = "medguard"
-        )
+conn = get_connection()
 
-st.title("Login/Sign in")
-options = st.selectbox("Login / Sign in: ", ["Login", "Sign in"])
+col1, col2 = st.columns([8,2])
+with col1:
+    st.title("Login/Sign in")
+    options = st.selectbox("Login / Sign in: ", ["Login", "Sign in"])
+
+with col2:
+    if st.button("Home Page"):
+        st.switch_page("pages/Home.py")
+
+
 if options == "Login":
 
     usernamee = st.text_input("Username")
     password = st.text_input("Password", type="password")
-    role = st.selectbox("Login as",["Patient", "Doctor"])
+    role = st.selectbox("Login as",["Patient", "Doctor", "Admin"])
 
     usernamee = usernamee.strip().lower()
 
@@ -35,28 +38,38 @@ if options == "Login":
                 st.success("Logged in as " + role)
                 if role == "Doctor":
                     st.switch_page("pages/Doctor.py")
-                else:
-                    st.switch_page("pages/Patient.py")
             else:
                 st.warning("Incorrect Username or Password ")
 
-        else:
+        elif role == "Admin":
             cur.execute("""
-                        select password from patient_login where username = (%s)
+                        select password from admin_login where username = (%s)
                     """, (usernamee,))
             x = cur.fetchone()
             if x[0] == password:
                 st.session_state.role = role
                 st.session_state.logged_in = True
                 st.success("Logged in as " + role)
-                if role == "Doctor":
-                    st.switch_page("pages/Doctor.py")
-                else:
-                    st.switch_page("pages/Patient.py")
+                if role == "Admin":
+                    st.switch_page("pages/Admin.py")
             else:
-                st.warning("User not found")
+                st.warning("Incorrect Username or Password")
+
+        else:
+            cur.execute("""
+                            select password from patient_login where username = (%s)
+                                """, (usernamee,))
+            x = cur.fetchone()
+            if x[0] == password:
+                st.session_state.role = role
+                st.session_state.logged_in = True
+                st.success("Logged in as " + role)
+                if role == "Admin":
+                    st.switch_page("pages/Patient   .py")
+            else:
+                st.warning("Incorrect Username or Password")
 else:
-    role = st.selectbox("Sign in as", ["Patient", "Doctor"])
+    role = st.selectbox("Sign in as", "Patient")
     name = st.text_input("UserName")
     choose_password = st.text_input("Password", type="password")
 
@@ -64,16 +77,12 @@ else:
     if st.button("Create Account"):
 
         cur = conn.cursor()
-        if role == "Doctor":
-            cur.execute("""
-                        INSERT INTO doctor_login(username, password)
-                        VALUES (%s, %s)
-                    """, (name, choose_password))
-        else:
+        if role == "Patient":
             cur.execute("""
                         INSERT INTO patient_login(username, password)
                         VALUES (%s, %s)
                     """, (name, choose_password))
+
 
         conn.commit()
         cur.close()
